ebb7afb0c7a2cd044eeb4defe199f568427e3d7e
root <root@ip-172-31-4-196.ap-south-1.compute.internal>
2024-02-27 21:55:21 +0000

    Updated by Delhi user

--- for_loop.py
+++ for_loop.py
@@ -1,10 +1,12 @@
 # writing code is great practise
 
 for i in range(6):
+
+    # the range of j is depend on i
     for j in range i:
+
+        # The below print statment used to print values in a row
         print("*", end=" ")
+
+    # The below print statement usend for the new line
     print()
-
-
-
-
